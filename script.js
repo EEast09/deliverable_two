ebb4e38f6a114ccc86767eab1155ea1bd835a859
Erinn East <erinn.east@gmail.com>
2022-06-14 17:46:47 -0400

    Almost Completed

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 let person = prompt("Welcome to GC mini golf! What is your name?", "Harry Potter");
 
 if (person != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + person + "! Would you like to play 3 or 6 holes?";
+  prompt("Hello " + person + "! Would you like to play 3 or 6 holes?");
+
+}
+let numberHoles = 
+numberHoles++
+let holes = prompt("Would you like to play 3 or 6 holes?");
+let total = 0
+for (let i = 0; i < holes; i++) {
+  let putts = Number (prompt(`How many putts for holes ${numberHoles}? (par: 3) `))
+  total += putts;
+}
+console.log(`${total}`);
+
+let totalPar;
+
+if (holes === 3){
+  totalPar = total - 9
+} else {
+  totalPar = total - 18
+}
+
+if (totalPar > 18){
+  console.log(`Nice try, ${person}... Your total par was: +(${totalPar}).`)
+}if (totalPar < 18){
+  console.log(`Great job, ${person}! Your total par was: -(${totalPar})`)
+} else {
+  console.log(`Good game, ${person}. Your total par was: 0."
+  `)
+}
+
+if (totalPar > 9){
+  console.log(`Nice try, ${person}... Your total par was: +(${totalPar}).`)
+}if (totalPar < 9){
+  console.log(`Great job, ${person}! Your total par was: -(${totalPar})`)
+} else {
+  console.log(`Good game, ${person}. Your total par was: 0."
+  `)
 }
