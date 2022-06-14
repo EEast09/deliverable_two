d26a5b81d0e83d664aa563a18fe657f5d2910c00
Erinn East <erinn.east@gmail.com>
2022-06-13 21:57:27 -0400

    Intoduction Prompt

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-console.log("Its working")
-console.log("This commit better work!")
+let person = prompt("Welcome to GC mini golf! What is your name?", "Harry Potter");
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person + "! Would you like to play 3 or 6 holes?";
+}
