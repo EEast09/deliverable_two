148f08c89e7d0517eea58f75211e08fda10be93a
Erinn East <erinn.east@gmail.com>
2022-06-17 15:51:39 -0400

    final

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let person = prompt("Welcome to GC mini golf! What is your name?", "Harry Potter");
 
 if (person != null) {
-  prompt("Hello " + person + "! Would you like to play 3 or 6 holes?");
+  
 
 }
-let numberHoles = 1
-numberHoles++
-let holes = Number (prompt("Would you like to play 3 or 6 holes?"));
+
+
+let holes = Number (prompt("Hello " + person + "! Would you like to play 3 or 6 holes?"));
 let total = 0
 for (let i = 0; i < holes; i++) {
-  let putts = Number (prompt(`How many putts for this Hole ${numberHoles}? (par: 3) `))
+  let putts = Number (prompt(`How many putts for this hole? (par: 3) `))
   total += putts;
 }
 console.log(`${total}`);
@@ -22,19 +22,19 @@ if(holes === 3){
   totalPar = total - 18
 }
 
-if (totalPar > 18){
-  console.log(`Nice try, ${person}... Your total par was: +(${totalPar}).`)
-}if (totalPar < 18){
-  console.log(`Great job, ${person}! Your total par was: -(${totalPar})`)
+if (totalPar > 18) {
+  console.log(`Nice try, ${person}... Your total par was: ${totalPar}.`)
+}if (totalPar < 18) {
+  console.log(`Great job, ${person}! Your total par was: ${totalPar}`)
 } else {
-  console.log(`Good game, ${person}. Your total par was: 0."
+  console.log(`Good game, ${person}. Your total par was: 0.)
   `)
 }
 
-if (totalPar > 9){
-  console.log(`Nice try, ${person}... Your total par was: +(${totalPar}).`)
-}if (totalPar < 9){
-  console.log(`Great job, ${person}! Your total par was: -(${totalPar})`)
+if (totalPar > 9) {
+  console.log(`Nice try, ${person}... Your total par was: ${totalPar}.`)
+}if (totalPar < 9) {
+  console.log(`Great job, ${person}! Your total par was: ${totalPar}`)
 } else {
   console.log(`Good game, ${person}. Your total par was: 0."
   `)
